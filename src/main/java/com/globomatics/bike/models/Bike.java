76e4161511430eea0044bbb9f03036786d26bbbb
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2019-05-24 23:37:22 +0530

    We imported the wrong Id annotation that caused issues while initializing the Bike entity

--- src/main/java/com/globomatics/bike/models/Bike.java
+++ src/main/java/com/globomatics/bike/models/Bike.java
@@ -2,11 +2,11 @@ package com.globomatics.bike.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.Date;
 
